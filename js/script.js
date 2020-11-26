ca34437f664a24f0944732a29f677137693e5047
roman <19roman23@gmail.com>
2020-11-26 16:36:57 +0200

    finished all block

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
-"use script";
-document.addEventListener("DOMContentLoaded", () => {
-  /*intro*/
-  let scrollBtn = document.getElementById("scrollBtn");
-  scrollBtn.addEventListener('click', scrollDown);
-  function scrollDown(event) {
+jQuery(document).ready(function(){
+  "use script";
+  /*scroll down btn*/
+  var scrollBtn = $("#scrollBtn");
+  scrollBtn.on("click", function (event){
     event.preventDefault();
-  }
+    var wHight = $(window).height();
+    $("html, body").animate({
+      scrollTop: wHight
+    }, 1000);
+  });
+  /*post block*/
+  var dropDownBtn = $(".drop__down--header");
+  var downCloseBtn = $(".header__close");
+  dropDownBtn.on("click", function(){
+    $(".header__close").toggleClass("header__close--active");
+  });
 });
